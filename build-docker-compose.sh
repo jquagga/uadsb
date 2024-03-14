415d3c8792439fe48a93965149856c586cddf654
Justin Settle <jus10@partlycloudy.org>
2024-03-14 16:12:11 -0400

    Support UUID

--- build-docker-compose.sh
+++ build-docker-compose.sh
@@ -2,6 +2,13 @@
 
 . ./.env
 
+#Put uuid in a file and then we can mount it in docker-compose for use
+if [ -n "${ULTRAFEEDER_UUID}" ]; then
+	echo "$ULTRAFEEDER_UUID" >uuid
+else
+	cat /proc/sys/kernel/random/uuid >uuid
+fi
+
 # First lets copy the templates into the config files
 cp docker-compose.tmpl docker-compose.yml
 cp piaware.tmpl piaware.conf
@@ -14,5 +21,6 @@ sed -i "s/ADSB_SDR_PPM/$ADSB_SDR_PPM/g" "docker-compose.yml"
 sed -i "s/FEEDER_LAT/$FEEDER_LAT/g" "docker-compose.yml"
 sed -i "s/FEEDER_LONG/$FEEDER_LONG/g" "docker-compose.yml"
 sed -i "s/FEEDER_ALT_M/$FEEDER_ALT_M/g" "docker-compose.yml"
+sed -i "s/ULTRAFEEDER_UUID/$ULTRAFEEDER_UUID/g" "docker-compose.yml"
 
 sed -i "s/PIAWARE_FEEDER_ID/$PIAWARE_FEEDER_ID/g" "piaware.conf"
